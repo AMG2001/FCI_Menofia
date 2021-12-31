74acbea438d26cab70236a839b4fe15e976d699e
mavicatech <mohamadamgad09@gmail.com>
2021-12-31 03:49:49 +0200

    Display page Finished`

---
+++ src/main/java/login/signUp/loginpage.java
@@ -0,0 +1,34 @@
+package login.signUp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class loginpage {
+
+
+    @FXML
+    private Button loginbutton;
+
+    @FXML
+    private TextField passworfdtextfield;
+
+    @FXML
+    private Button signupbuuton;
+
+    @FXML
+    private TextField usernametextfield;
+
+    @FXML
+    void signinbuttonfunction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signupbuutonfunction(ActionEvent event) {
+
+    }
+
+}
+
